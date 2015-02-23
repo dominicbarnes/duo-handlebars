4416000cb2694dca4756300b4e6f3da387496798
Dominic Barnes <dominic@segment.io>
2015-02-23 11:08:13 -0700

    refactor

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 // dependencies
-var extend = require("extend");
-var fs = require("co-fs");
-var Handlebars = require("handlebars");
-var runtimePath = require.resolve("handlebars/dist/handlebars.runtime.js");
-var util = require("util");
+var debug = require('debug')('duo-handlebars');
+var extend = require('extend');
+var fs = require('co-fs');
+var Handlebars = require('handlebars');
+var runtimePath = require.resolve('handlebars/dist/handlebars.runtime.js');
+var util = require('util');
 
 // default config options
 var defaults = {
-    extension: "hbs"
+  extension: 'hbs'
 };
 
 // template used for outputting the file to the duo builder
 var format = [
-    "var Handlebars = require('handlebars-runtime');",
-    "module.exports = Handlebars.template(%s);"
-].join("\n");
+  'var Handlebars = require("handlebars-runtime");',
+  'module.exports = Handlebars.template(%s);'
+].join('\n');
 
 
 module.exports = function (opts) {
-    var options = extend({}, defaults, opts);
-    var included = false;
+  var options = extend(true, {}, defaults, opts);
+  debug('initialized with options', opts);
 
-    // extract this option (the rest are given directly to Handlebars)
-    var extension = options.extension;
-    delete options.extension;
+  // extract this option (the rest are given directly to Handlebars)
+  var extension = options.extension;
+  delete options.extension;
 
-    return function *handlebars(file) {
-        if (file.type !== extension) return;
-        file.type = "js";
+  return function *handlebars(file) {
+    if (file.type !== extension) return;
+    file.type = 'js';
 
-        if (!included) {
-            var runtime = yield fs.readFile(runtimePath, "utf8");
-            file.include("handlebars-runtime", runtime);
-            included = true;
-        }
+    if (!file.included('handlebars-runtime')) {
+      var runtime = yield fs.readFile(runtimePath, 'utf8');
+      file.include('handlebars-runtime', runtime);
+      debug('runtime added');
+    }
 
-        var tpl = Handlebars.precompile(file.src, options);
-        file.src = util.format(format, tpl);
-    };
+    var tpl = Handlebars.precompile(file.src, options);
+    debug('%s compiled', file.id);
+    file.src = util.format(format, tpl);
+  };
 };
--- test/index.js
+++ test/index.js
@@ -1,47 +1,47 @@
-var assert = require("assert");
-var Duo = require("duo");
-var fs = require("co-fs");
-var handlebars = require("../");
-var path = require("path");
-var thunkify = require("thunkify");
-var rm = thunkify(require("rimraf"));
-var vm = require("vm");
+var assert = require('assert');
+var Duo = require('duo');
+var fs = require('co-fs');
+var handlebars = require('../');
+var path = require('path');
+var thunkify = require('thunkify');
+var rm = thunkify(require('rimraf'));
+var vm = require('vm');
 
-var fixture = path.join.bind(path, __dirname, "fixtures");
+var fixture = path.join.bind(path, __dirname, 'fixtures');
 
 after(function* () {
-    yield rm(fixture("components"));
+  yield rm(fixture('components'));
 });
 
-describe("duo-handlebars", function () {
-    it("should be a function", function () {
-        assert(typeof handlebars === "function");
-    });
+describe('duo-handlebars', function () {
+  it('should be a function', function () {
+    assert(typeof handlebars === 'function');
+  });
 
-    it("should return a function", function () {
-        assert(typeof handlebars() === "function");
-    });
+  it('should return a function', function () {
+    assert(typeof handlebars() === 'function');
+  });
 
-    it("should compile handlebars templates", function* () {
-        var root = fixture();
+  it('should compile handlebars templates', function* () {
+    var root = fixture();
 
-        var duo = new Duo(root)
-            .use(handlebars())
-            .entry("example.js");
+    var duo = new Duo(root)
+      .use(handlebars())
+      .entry('example.js');
 
-        var js = yield duo.run();
-        var tpl = evaluate(js).main;
+    var js = yield duo.run();
+    var tpl = evaluate(js).main;
 
-        var actual = tpl({ name: "World" });
-        var expected = yield fs.readFile(fixture("example.html"), "utf8");
-        assert(actual === expected);
-    });
+    var actual = tpl({ name: 'World' });
+    var expected = yield fs.readFile(fixture('example.html'), 'utf8');
+    assert(actual === expected);
+  });
 });
 
 
-function evaluate(js){
-    var ctx = { window: {}, document: {} };
-    vm.runInNewContext('main =' + js + '(1)', ctx, 'main.vm');
-    vm.runInNewContext('require =' + js + '', ctx, 'require.vm');
-    return ctx;
+function evaluate(js) {
+  var ctx = { window: {}, document: {} };
+  vm.runInNewContext('main =' + js + '(1)', ctx, 'main.vm');
+  vm.runInNewContext('require =' + js + '', ctx, 'require.vm');
+  return ctx;
 }
